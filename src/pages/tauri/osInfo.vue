469c7784f568942f6e7e42bc982fd12a143511b9
tuyongtao <tuyongtao@mantun.com>
2025-02-14 13:46:10 +0800

    feat:去除无用的权限，页面美化

--- src/pages/tauri/osInfo.vue
+++ src/pages/tauri/osInfo.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
-    <h1>操作系统信息</h1>
-    <p>platform:{{ currentPlatform }}</p>
-    <p>family:{{ familyInfo }}</p>
-    <p>exeExtension:{{ exeExtensionInfo }}</p>
-    <p>arch:{{ archInfo }}</p>
-    <p>hostname:{{ hostnameInfo }}</p>
-    <p>locale:{{ localeInfo }}</p>
-    <p>type:{{ typeInfo }}</p>
-    <p>version:{{ versionInfo }}</p>
+  <div class="bg-white shadow-md rounded-lg p-6 max-w-md mx-auto mt-10">
+    <h1 class="text-2xl font-semibold text-gray-800 mb-4">操作系统信息</h1>
+    <p class="text-gray-700">
+      platform: <span class="font-medium">{{ currentPlatform }}</span>
+    </p>
+    <p class="text-gray-700">
+      family: <span class="font-medium">{{ familyInfo }}</span>
+    </p>
+    <p class="text-gray-700">
+      exeExtension: <span class="font-medium">{{ exeExtensionInfo }}</span>
+    </p>
+    <p class="text-gray-700">
+      arch: <span class="font-medium">{{ archInfo }}</span>
+    </p>
+    <p class="text-gray-700">
+      hostname: <span class="font-medium">{{ hostnameInfo }}</span>
+    </p>
+    <p class="text-gray-700">
+      locale: <span class="font-medium">{{ localeInfo }}</span>
+    </p>
+    <p class="text-gray-700">
+      type: <span class="font-medium">{{ typeInfo }}</span>
+    </p>
+    <p class="text-gray-700">
+      version: <span class="font-medium">{{ versionInfo }}</span>
+    </p>
   </div>
 </template>
 
